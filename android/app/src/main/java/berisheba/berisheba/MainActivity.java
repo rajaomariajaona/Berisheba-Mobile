3e4f80f710591b507fae300f86994eb8791e3659
rajaomariajaona <rajaomariajaona11@gmail.com>
2019-12-17 15:58:17 +0300

    Config icons

---
+++ android/app/src/main/java/berisheba/berisheba/MainActivity.java
@@ -0,0 +1,16 @@
+package berisheba.berisheba;
+
+import androidx.annotation.NonNull;
+
+import java.lang.reflect.Method;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
