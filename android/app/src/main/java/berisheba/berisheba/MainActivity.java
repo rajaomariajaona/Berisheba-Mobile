7228cb7eb17c60dfb6e46c83e15aeceaad8e6411
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-02-21 05:43:25 +0300

    Update fix

--- android/app/src/main/java/berisheba/berisheba/MainActivity.java
+++ android/app/src/main/java/berisheba/berisheba/MainActivity.java
@@ -1,20 +1,13 @@
 package berisheba.berisheba;
 
 import androidx.annotation.NonNull;
-
-import java.lang.reflect.Method;
-
-
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
     GeneratedPluginRegistrant.registerWith(flutterEngine);
-//    new MethodChannel();
-//    MethodChannel
   }
 }
