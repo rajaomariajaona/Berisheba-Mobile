3b25b2ad7fa3d24a56a15f470ae1bc69cee0a86a
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-03-08 12:04:23 +0300

    Switch to temporary about Menu drawer

--- lib/home_page/menu_drawer.dart
+++ lib/home_page/menu_drawer.dart
@@ -1,5 +1,8 @@
 import 'package:berisheba/states/config.dart';
+import 'package:berisheba/states/parametres.dart';
+import 'package:berisheba/tools/formatters/second_to_string_formatter.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MenuDrawer extends StatelessWidget {
   const MenuDrawer({Key key}) : super(key: key);
@@ -36,9 +39,55 @@ class MenuDrawer extends StatelessWidget {
                   SizedBox(
                     height: 15,
                   ),
-                  Flexible(
-                    child: _NotificationSettigns(),
+                  Expanded(
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: <Widget>[
+                            Text(
+                              "'Dieu est avec toi dans tout ce que tu fais.'",
+                              style: TextStyle(
+                                fontFamily: "Poppins",
+                                fontSize: 25,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(
+                              height: 15,
+                            ),
+                            Text(
+                              "Genèse 21 : 22",
+                              style: TextStyle(
+                                fontFamily: "Poppins",
+                                fontStyle: FontStyle.italic,
+                                fontSize: 20,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
+                  Divider(),
+                  ListTile(
+                    leading: Icon(Icons.info_outline),
+                    title: Text("A propos"),
+                    onTap: () async {
+                      await showAboutDialog(
+
+                        context: context,
+                        children: <Widget>[
+                          Text("Adresse: Ambatofotsy-Gara PK21 RN7"),
+                          Text("Contact: 034 11 641 54"),
+                        ],
+                        applicationVersion: "1.0",
+                        applicationIcon: Image.asset("assets/logo.png",width: MediaQuery.of(context).size.width / 3,)
+                      );
+                    },
+                  )
                 ],
               ),
             ),
@@ -46,6 +95,50 @@ class MenuDrawer extends StatelessWidget {
         )
       ],
     ));
+  }
+}
+
+class ParametresFuture extends StatelessWidget {
+  const ParametresFuture({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: Config.secondaryBlue,
+        child: SafeArea(
+          top: true,
+          child: Flex(
+            direction: Axis.vertical,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10),
+                  ),
+                  color: Config.primaryBlue,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Container(
+                    child: Image.asset("assets/logo.png"),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Flexible(
+                child: _NotificationSettigns(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
 
@@ -59,7 +152,18 @@ class _NotificationSettigns extends StatefulWidget {
 }
 
 class __NotificationSettignsState extends State<_NotificationSettigns> {
+  SharedPreferences instance;
   bool notificationOn = false;
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      instance = await SharedPreferences.getInstance();
+      notificationOn = instance.getBool(Parametres.notification);
+      setState(() {});
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -67,22 +171,80 @@ class __NotificationSettignsState extends State<_NotificationSettigns> {
         leading: Icon(Icons.settings),
         title: const Text("Parametres"),
         children: <Widget>[
-          ExpansionTile(
+          ListTile(
             leading: Switch(
               value: notificationOn,
-              onChanged: (bool val) {
+              onChanged: (bool val) async {
+                if (instance == null) {
+                  instance = await SharedPreferences.getInstance();
+                }
+                await instance.setBool(Parametres.notification, val);
                 setState(() {
                   notificationOn = val;
                 });
               },
             ),
-            trailing: notificationOn ? null: SizedBox(),
+            trailing: notificationOn
+                ? IconButton(icon: Icon(Icons.settings), onPressed: () async {})
+                : SizedBox(),
             title: ListTile(
               title: const Text("Notification"),
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class NotificationParametresDialog extends StatefulWidget {
+  @override
+  _NotificationParametresDialogState createState() =>
+      _NotificationParametresDialogState();
+}
+
+class _NotificationParametresDialogState
+    extends State<NotificationParametresDialog> {
+  List<int> duration;
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              for (var index = 0; index < duration.length; index++)
+                ListTile(
+                  title: Text(
+                      "${SecondToStringFormatter.format(duration[index])}"),
+                  trailing: IconButton(
+                      icon: Icon(Icons.close),
+                      onPressed: () async {
+                        setState(() {
+                          duration.removeAt(index);
+                        });
+                      }),
+                )
+            ],
+          ),
+        ),
+      ),
+      actions: <Widget>[
+        IconButton(
+            icon: Icon(Icons.check),
+            onPressed: () async {
+              await SharedPreferences.getInstance().then((instance) {
+                instance.setStringList(Parametres.notificationValues,
+                    duration.map((duree) => duree.toString()));
+              });
+              Navigator.of(context).pop();
+            }),
+        IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () async {
+              Navigator.of(context).pop();
+            }),
+      ],
     );
   }
 }
